ce6acf6e8d4b1e9a3caf13e314318cb1a866539a
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-11-17 13:38:17 +0530

    Test #1

--- public/main.js
+++ public/main.js
@@ -1,33 +1,19 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const localServerApp = express();
-const PORT = 8088;
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+const isDev = require('electron-is-dev')
 
-const startLocalServer = (done) => {
-  localServerApp.use(express.json({ limit: "100mb" }));
-  localServerApp.use(cors());
-  localServerApp.use(express.static('./build/'));
-  localServerApp.listen(PORT, async () => {
-    console.log("Server Started on PORT ", PORT);
-    done();
-  });
-};
-
-function createWindow() {
+const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
   // and load the index.html of the app.
-  //   mainWindow.loadFile('index.html')
-  mainWindow.loadURL("http://localhost:" + PORT);
+  mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -37,18 +23,18 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  startLocalServer(createWindow);
+  createWindow()
 
-  app.on("activate", function () {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
-});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
